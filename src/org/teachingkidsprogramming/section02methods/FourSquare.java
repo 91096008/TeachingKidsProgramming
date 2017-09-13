4f798242ffc92a56367b281c2f8c3e100c008038
91096008 <91096008@mvusd.net>
2017-09-13 07:59:07 -0700

    finished Houses.Java

--- src/org/teachingkidsprogramming/section02methods/FourSquare.java
+++ src/org/teachingkidsprogramming/section02methods/FourSquare.java
@@ -1,10 +1,12 @@
 package org.teachingkidsprogramming.section02methods;
 
+import org.teachingextensions.logo.Tortoise;
+
 public class FourSquare
 {
   public static void main(String[] args)
   {
-    //  Show the tortoise --#1
+    Tortoise.show();
     //  Make the tortoise move as fast as possible --#7
     //  Do the following 4 times --#8.1
     //      drawSquare (recipe below) --#6.1
@@ -12,6 +14,7 @@ public class FourSquare
     //      Do the following 4 times --#5.1
     //          Change the pen color of the line the tortoise draws to a random color --#3
     //          Move the tortoise 50 pixels --#2
+    Tortoise.move(50);
     //          Turn the tortoise 90 degrees to the right --#4
     //      End Repeat --#5.2
     //      ------------- End of drawSquare recipe --#6.3
