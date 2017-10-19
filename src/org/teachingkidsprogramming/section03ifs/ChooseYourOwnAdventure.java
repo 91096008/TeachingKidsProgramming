e0fc83215b5a9fefca22f1c05af8ff8b10c120ea
91096008 <91096008@mvusd.net>
2017-10-19 13:45:55 -0700

    I edited choose your own adventure

--- src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
+++ src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
@@ -65,18 +65,23 @@ public class ChooseYourOwnAdventure
   }
   private static void pourIntoToilet()
   {
-    //            pourIntoToilet (recipe below) --#12.1
-    //            ------------- Recipe for pourIntoToilet --#12.2
     MessageBox.showMessage(
         "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste.");
-    MessageBox.askForTextInput("Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?");
+    String answer = MessageBox.askForTextInput("Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?");
     //               If they answer "yes" --#13.1
-    //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
+    if ("yes".equalsIgnoreCase(answer))
+    {
+      //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
+    }
     //               Otherwise, if they answer "heck yes" --#13.2
-    //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#15
-    //               Otherwise, if they answer anything else --#13.3
-    //                    endStory --#16
-    //            ------------- End of pourIntoToilet recipe --#12.3
+    else if ("heck yes".equalsIgnoreCase(answer))
+    {
+      //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#15
+      //               Otherwise, if they answer anything else --#13.3
+      {
+        //                    endStory --#16
+      }
+    }
   }
   private static void wakeUp()
   {
